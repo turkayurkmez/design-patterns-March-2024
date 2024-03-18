690a65b8014420c9917a7a68e6251c325ea5940b
Türkay Ürkmez <turkay@turkayurkmez.com>
2024-03-18 16:00:19 +0300

    Abstracd and Inteface explained

---
+++ OverviewOfOOP/AbstractANDInterface/Document.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractANDInterface
+{
+    public abstract class Document
+    {
+        public string Title { get; set; }
+        public string Owner { get; set; }
+        public void Copy(string from, string to)
+        {
+            //yazıldığını düşünün 
+        }
+
+        public void Move(string from, string to)
+        {
+            //yazıldığını düşünün 
+
+        }
+
+        public abstract void Save(string path);
+        public abstract void Open(string path);
+
+
+    }
+
+    public interface IPrintable
+    {
+        void Print();
+    }
+
+    public class ExcelDocument : Document, IPrintable
+    {
+
+
+        public override void Open(string path)
+        {
+            Console.WriteLine($"{GetType().Name} açıldı");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{GetType().Name} çıktı alındı");
+        }
+
+        public override void Save(string path)
+        {
+            Console.WriteLine($"{GetType().Name} dosyası kaydedildi");
+
+        }
+    }
+
+    public class PDFDocument : Document
+    {
+        public override void Open(string path)
+        {
+            Console.WriteLine($"{GetType().Name} dosyası açıldı");
+
+        }
+
+
+
+        public override void Save(string path)
+        {
+            Console.WriteLine($"{GetType().Name} dosyası kaydedildi");
+
+        }
+    }
+
+
+
+    public class WordDocument : Document, IPrintable
+    {
+        public override void Open(string path)
+        {
+            Console.WriteLine($"{GetType().Name} dosyası açıldı");
+
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{GetType().Name} çıktı alındı");
+
+        }
+
+        public override void Save(string path)
+        {
+            Console.WriteLine($"{GetType().Name} dosyası kaydedildi");
+
+        }
+    }
+
+
+    public class PrintComponent
+    {
+        public void Print(IPrintable document)
+        {
+            document.Print();
+        }
+    }
+
+}
--- OverviewOfOOP/AbstractANDInterface/Program.cs
+++ OverviewOfOOP/AbstractANDInterface/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using AbstractANDInterface;
+
+Document document = new ExcelDocument();
+document.Save("");
+
+
