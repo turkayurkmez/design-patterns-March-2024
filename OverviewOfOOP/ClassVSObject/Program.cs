7cd4578ac46e4013444be3d7b580338c6efd1164
Türkay Ürkmez <turkay@turkayurkmez.com>
2024-03-18 12:11:09 +0300

    init repo & what is oop terminology

---
+++ OverviewOfOOP/ClassVSObject/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassVSObject;
+
+Console.WriteLine("Hello, World!");
+
+Student student1 = new Student();
+Student student2 = new Student();
+student1.Name = "Türkay";
+student1.LastName = "Ürkmez";
+
+Student student3 = student1;
+student3.Name = "Derya";
+
+Console.WriteLine(student1.Name);
---
+++ OverviewOfOOP/ClassVSObject/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassVSObject
+{
+    public class Student
+    {
+        public string Name;
+        public string LastName;
+
+        //Her öğrencinin ................ vardır
+
+        //Her öğrenciyle .... yapılır / her öğrenci ..... yapar
+
+    }
+}
