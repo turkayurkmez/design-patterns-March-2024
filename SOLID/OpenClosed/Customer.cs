530159bb411b4aed86fc6a99c3c162a92e88e835
Türkay Ürkmez <turkay@turkayurkmez.com>
2024-03-19 11:07:01 +0300

    Open Closed

---
+++ SOLID/OpenClosed/Customer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosed
+{
+    public enum CardType
+    {
+        Standard,
+        Silver,
+        Gold
+    }
+    public class Customer
+    {
+        public string Name { get; set; }
+        public CardType Card { get; set; }
+    }
+
+    public class OrderManagement
+    {
+        public Customer Customer { get; set; }
+        public double GetTotalPrice(double price)
+        {
+            switch (Customer.Card)
+            {
+                case CardType.Standard:
+                    return price * 0.95;
+                case CardType.Silver:
+                    return price * 0.90;
+                case CardType.Gold:
+                    return price * 0.85;
+                default:
+                    return price;
+
+            }
+        }
+    }
+}
---
+++ SOLID/OpenClosed/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenClosed;
+
+Console.WriteLine("Hello, World!");
+
+//Açık/Kapalı Prensibi
+//Bir nesne .....gelişime.... açık ......değişime.... kapalı olmalıdır
+
+Customer customer = new Customer { Name = "Türkay", Card = CardType.Gold };
+OrderManagement orderManagement = new OrderManagement { Customer = customer };
+
+Console.WriteLine(orderManagement.GetTotalPrice(1000));
