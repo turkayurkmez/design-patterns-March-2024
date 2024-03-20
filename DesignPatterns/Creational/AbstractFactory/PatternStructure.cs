b3414651f64fea5db75d0bad8db512e6cd72458e
Türkay Ürkmez <turkay@turkayurkmez.com>
2024-03-20 11:05:33 +0300

    Factory method % abstract factory expalined

---
+++ DesignPatterns/Creational/AbstractFactory/PatternStructure.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+
+    public interface IDbConnection
+    {
+        void Connect();
+    }
+    public class SQLConnection : IDbConnection
+    {
+        public void Connect()
+        {
+            Console.WriteLine("SQL'e bağlandım");
+        }
+    }
+
+    public class OracleConnection : IDbConnection
+    {
+        public void Connect()
+        {
+            Console.WriteLine("Oracle'a bağlandım");
+
+        }
+
+    }
+
+    public interface IDbCommand
+    {
+        void Execute();
+    }
+    public class SqlCommand : IDbCommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("SQL'den sorgu çektim");
+
+        }
+    }
+    public class OracleCommand : IDbCommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Oracle'den sorgu çektim");
+
+        }
+    }
+
+    public interface IAbstractDataToolsFactory
+    {
+        public IDbConnection CreateConnection();
+        public IDbCommand CreateCommand();
+    }
+
+    public class OracleDataToolsFactory : IAbstractDataToolsFactory
+    {
+        public IDbCommand CreateCommand()
+        {
+            return new OracleCommand();
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new OracleConnection();
+        }
+    }
+
+    public class SqlDataToolsFactory : IAbstractDataToolsFactory
+    {
+        public IDbCommand CreateCommand()
+        {
+            return new SqlCommand();
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new SQLConnection();
+        }
+    }
+
+    public class DataTools<T> where T : IAbstractDataToolsFactory, new()
+    {
+        private T factory = new T();
+        public void GetData()
+        {
+            var connection = factory.CreateConnection();
+            var command = factory.CreateCommand();
+
+            connection.Connect();
+            command.Execute();
+        }
+
+    }
+}
---
+++ DesignPatterns/Creational/AbstractFactory/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AbstractFactory;
+
+Console.WriteLine("Hello, World!");
+/*
+ *  Belirli bir seçime (ya da duruma) bağlı olarak gerekecek nesnelerin belli olduğu bir durum düşünün. 
+ *  Örneğin, Ms SQL Server ya da Oracle gibi iki ayrı paltformdan biriyle çalışacak bir uygulama düşünelim.
+ *    Oracle -> OracleCommand, OracleConnection, OracleDataAdapter
+ *    SQL    -> SQLCommand, SQLConnection, SQLDataAdapter
+ *    
+ *    
+ *    DataTools<MS> toolsForMicrosoft = new DataTools<MS>();
+ *    toolsForMicrosoft.GetData();
+ */
+
+OracleDataToolsFactory oracle = new OracleDataToolsFactory();
+var oracleConnection = oracle.CreateConnection();
+var oracleCommand = oracle.CreateCommand();
+
+oracleConnection.Connect();
+oracleCommand.Execute();
+
+DataTools<SqlDataToolsFactory> toolsForSQL = new DataTools<SqlDataToolsFactory>();
+toolsForSQL.GetData();
