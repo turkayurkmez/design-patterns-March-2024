8b86b1765b5339f1d8819eb1f812f4c336673059
Türkay Ürkmez <turkay@turkayurkmez.com>
2024-03-20 12:05:49 +0300

    Builder pattern explained

---
+++ DesignPatterns/Creational/Builder/PatternStructure.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    public interface IReportBuilder
+    {
+        void BuildHeader();
+        void BuildContent();
+        void BuildSign();
+    }
+
+    public class Report
+    {
+        //Raporu oluşturan header, content ve sign parçalarının her biri string bir değer:
+        List<string> parts = new List<string>();
+        public void Add(string part)
+        {
+            parts.Add(part);
+        }
+
+        public void ShowReport()
+        {
+            parts.ForEach(part => Console.WriteLine(part));
+        }
+
+
+
+    }
+
+    public class ReportBuilder : IReportBuilder
+    {
+
+        private Report report = new Report();
+        public void BuildContent()
+        {
+            report.Add("Raporun içeriği oluşturuldu....");
+        }
+
+        public void BuildHeader()
+        {
+            report.Add("Başlık oluşturuldu");
+        }
+
+        public void BuildSign()
+        {
+            report.Add("İmzalar eklendi");
+
+        }
+
+        public Report Build()
+        {
+            BuildHeader();
+            BuildContent();
+            BuildSign();
+
+            return report;
+        }
+
+    }
+
+
+}
--- DesignPatterns/Creational/Builder/Program.cs
+++ DesignPatterns/Creational/Builder/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Builder;
+
 Console.WriteLine("Hello, World!");
+/*
+ * Bir nesnenin her değişen parçası karmaşık işlemler ile oluşmaktadır. Bu parçalar kendi içlerinde değişiyorsa; ayrı ayrı nesneler tarafından oluşturulup birleştirilmesi tercih edilebilir. 
+ * 
+ * Örnek:
+ * 
+ *    Büyük bir şirketin tüm rapor ihtiyaçlarını karşılaycak bir uygulama düşünün.
+ *        - Rapor başlığı 
+ *        - Rapor verisi
+ *        - Rapor Grafikleri
+ *        
+ *        
+ *   ReportBuilder reportBuilder = new ReportBuilder();
+ *   var report =  reportBuilder.Build();
+ *   
+ *   
+ *   
+ *        
+ */
+
+ReportBuilder reportBuilder = new ReportBuilder();
+
+var report = reportBuilder.Build();
+report.ShowReport();
